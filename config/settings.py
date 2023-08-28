a7a02ab832c7f4c5bb8cf08e897a95f7fe886d44
duffing09 <duffing18@gmail.com>
2023-08-28 11:28:00 -0400

    log in stuff

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'album_list'
--- config/urls.py
+++ config/urls.py
@@ -14,20 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# import various modules and functions needed for URLs and views. admin used for django admin interface, path used to define url patterns. views refers to views you've defined in django app
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django_music import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+# defining url patterns for application. each 'path' function call defines a URL pattern with its corresponding view function. first pattern links to django admin interface.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.album_list, name="album_list"),
     path('albums/<int:pk>/', views.album_detail, name='album_detail'),
     path('album/new', views.album_new, name='album_new'),
     path('album/<int:pk>/edit/', views.album_edit, name='album_edit'),
-    path('album/<pk>/remove/', views.album_remove, name='album_remove')
+    path('album/<pk>/remove/', views.album_remove, name='album_remove'),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
 
+# checks whether application is in debug mode. if it is it adds url pattern for servnc static files.
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# this cod sets up URL patterns for your django application. When a user visits a specific URL, the corresponding view function from your views.py file is called to generate the appropriate HTML content. The urlpatterns list maps URLs to views, enabling navigation within my application. The 'if settings.DEBUG' block ensures that static files are served during development.
